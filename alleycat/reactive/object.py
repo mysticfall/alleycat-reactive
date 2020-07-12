1cc2491bdeefc3f968c36e92912da335d51793b3
Xavier Cho <mysticfallband@gmail.com>
2020-07-12 19:31:55 +0900

    Restore class method version of observe() for consistency

--- alleycat/reactive/object.py
+++ alleycat/reactive/object.py
@@ -1,8 +1,9 @@
 from typing import TypeVar
 
+from rx import Observable
 from rx.core.typing import Disposable
 
-from alleycat.reactive import extend, ReactiveProperty, PreModifier, PostModifier, dispose
+from alleycat.reactive import observe, extend, ReactiveProperty, PreModifier, PostModifier, dispose
 
 T = TypeVar("T")
 
@@ -16,6 +17,9 @@ class ReactiveObject(Disposable):
             pre_modifier: PreModifier = None,
             post_modifier: PostModifier = None) -> ReactiveProperty[T]:
         return extend(cls, name, pre_modifier, post_modifier)
+
+    def observe(self, name: str) -> Observable:
+        return observe(self, name)
 
     def dispose(self) -> None:
         dispose(self)
