c4161f2538b8099684c281dd7069dead525163d7
Xavier Cho <mysticfallband@gmail.com>
2020-07-21 07:37:41 +0900

    Add missing install_requires, bumping version to 0.1.1
    Use pipenv-setup to synchronize Pipenv and setup.py.

--- setup.py
+++ setup.py
@@ -1,21 +1,22 @@
-import setuptools
+from setuptools import find_packages, setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name="alleycat-reactive",
-    version="0.1",
+    version="0.1.1",
     author="Xavier Cho",
     author_email="mysticfallband@gmail.com",
     description="A simple Python library to provide an API to implement the Reactive Object Pattern (ROP).",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/mysticfall/alleycat-reactive",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
+    install_requires=["returns==0.14.0", "rx==3.1.1", "typing-extensions==3.7.4.2"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ]
+    ],
 )
