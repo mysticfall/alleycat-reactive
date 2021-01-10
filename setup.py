69ff2f283627a6c613b084677be707234b29164c
Xavier Cho <mysticfallband@gmail.com>
2021-01-10 17:21:36 +0900

    Fix setup tool settings concerning namespace package

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
-from setuptools import find_packages, setup
+from setuptools import find_namespace_packages, setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
     name="alleycat-reactive",
-    version="0.4.6",
+    version="0.4.7",
     author="Xavier Cho",
     author_email="mysticfallband@gmail.com",
     description="A simple Python library to provide an API to implement the Reactive Object Pattern (ROP).",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/mysticfall/alleycat-reactive",
-    packages=find_packages(),
+    packages=find_namespace_packages(include=["alleycat.*"]),
     install_requires=["returns==0.15.0", "rx==3.1.1"],
     classifiers=[
         "Programming Language :: Python :: 3",
